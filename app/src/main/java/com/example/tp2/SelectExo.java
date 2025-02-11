ab71354a5ed2e428916e761d03f259f16cf567c1
Vincent <flyber11@gmail.com>
2025-02-11 15:45:03 +0100

    start ex1

--- app/src/main/java/com/example/tp2/SelectExo.java
+++ app/src/main/java/com/example/tp2/SelectExo.java
@@ -22,52 +22,11 @@ public class SelectExo extends AppCompatActivity {
             return insets;
         });
 
-        Button buttonExo2 = findViewById(R.id.button_exo2);
+        Button buttonExo2 = findViewById(R.id.button_exo1);
         buttonExo2.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, MainActivity.class); //pas bien compris pour quoi il faut spécifier la classe si jamais diapo 61
+            Intent intent = new Intent(SelectExo.this, MainActivity.class);
             startActivity(intent);
         });
 
-        Button buttonExo3 = findViewById(R.id.button_exo3);
-        buttonExo3.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo3.class);
-            startActivity(intent);
-        });
-
-        Button buttonExo4 = findViewById(R.id.button_exo4);
-        buttonExo4.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo4.class);
-            startActivity(intent);
-        });
-
-        Button buttonExo5 = findViewById(R.id.button_exo5);
-        buttonExo5.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo5.class);
-            startActivity(intent);
-        });
-
-        Button buttonExo6 = findViewById(R.id.button_exo6);
-        buttonExo6.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo6.class);
-            startActivity(intent);
-        });
-
-        Button buttonExo7 = findViewById(R.id.button_exo7);
-        buttonExo7.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo7.class);
-            startActivity(intent);
-        });
-
-        Button buttonExo8 = findViewById(R.id.button_exo8);
-        buttonExo8.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo8.class);
-            startActivity(intent);
-        });
-
-        Button buttonExo9 = findViewById(R.id.button_exo9);
-        buttonExo9.setOnClickListener(v -> {
-            Intent intent = new Intent(SelectExo.this, Exo9.class);
-            startActivity(intent);
-        });
     }
 }
